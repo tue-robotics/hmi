0fa034e6d009a7fb47b6b5a2c1b1be5feb9615dd
Rein Appeldoorn <rein@ruvu.nl>
2017-10-12 10:49:47 +0200

    Refactoring and cleanup

--- hmi/src/hmi/grammar_parser.py
+++ hmi/src/hmi/grammar_parser.py
@@ -6,13 +6,16 @@
 This means that sentences can be generated (and auto-completed), according to this grammar.
 Moreover, sentences can be parsed according to the same rules.
 
-See https://www.tutorialspoint.com/automata_theory/context_free_grammar_introduction.htm and https://en.wikipedia.org/wiki/Context-free_grammar for an introduction to context free grammars.
+See https://www.tutorialspoint.com/automata_theory/context_free_grammar_introduction.htm and
+https://en.wikipedia.org/wiki/Context-free_grammar for an introduction to context free grammars.
 
-If there is a rule "A -> one", then that means that to generate something according to rule A, the generated sentence is "one"
+If there is a rule "A -> one", then that means that to generate something according to rule A, the generated sentence is
+"one"
 In this example "A" is the lname. lname stands for left name, as it's on the left of the arrow.
 Sentences are produced and parsed from left to right.
 
-There can be multiple lines in the grammar definition file with the same lname, which simply add ways to produce/parse sentences for that lname.
+There can be multiple lines in the grammar definition file with the same lname, which simply add ways to produce/parse
+sentences for that lname.
 
 Rules can refer to each other via their lname.
 If a rule A defines a way to start a sentence and refers to B, that means the completion of rule A is one via rule B.
@@ -46,26 +49,12 @@ When the Tree is completely parsed, the collected variables and their assignment
 This returns a string. However, this string represents a (nested) dictionary that maps a variable to a value.
 
 Semantics describe what a sentence means. In this case, it describes what action to perform and with what to perform it.
-
-The semantics are returned to whomever called GrammarParser.parse(...), usually the REPL on console.py.
-The REPL sends the semantics to the action_server, which grounds the semantics by implementing the actions.
 """
 
 import random
 import re
 import yaml
 from yaml import MarkedYAMLError
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 class Option:
@@ -108,31 +97,8 @@ class Option:
 
             yield opt
 
-    def pretty_print(self, level=0):
-        # print self, level
-        tabs = level * "    "
-        ret = "\n"
-        ret += tabs + "Option(lsemantic='{lsem}', conjs=[".format(lsem=self.lsemantic)
-        for conj in self.conjuncts:
-            # ret += "\n"
-            # ret += tabs + "    " + "{c},".format(c=conj)
-            ret += " "
-            ret += conj.pretty_print()
-        ret += "])"
-        return ret
-
-    def graphviz_id(self):
-        return "Option '{lsem}'".format(lsem=self.lsemantic).replace('"', '').replace(":", "")
-
-    def to_graphviz(self, graph):
-        for conj in self.conjuncts:
-            graph.edge(self.graphviz_id(), conj.graphviz_id())
-            conj.to_graphviz(graph)
-
 
 # ----------------------------------------------------------------------------------------------------
-
-
 
 
 class Conjunct:
@@ -152,25 +118,13 @@ class Conjunct:
 
     def __eq__(self, other):
         if isinstance(other, Conjunct):
-            return self.name == other.name and self.rsemantic == other.rsemantic and self.is_variable == other.is_variable
+            return self.name == other.name and \
+                   self.rsemantic == other.rsemantic and \
+                   self.is_variable == other.is_variable
         return False
-
-    def pretty_print(self, level=0):
-        if self.is_variable or "$" in self.name:
-            prefix = self.rsemantic + "="
-            return self.rsemantic + "=" + self.name  # + str(self)
-        else:
-            return bcolors.OKGREEN + self.name + bcolors.ENDC  # + str(self)
-
-    def graphviz_id(self):
-        return "Conjunct {name}".format(name=self.name)
-
-    def to_graphviz(self, graph):
-        graph.node(self.graphviz_id())
 
 
 # ----------------------------------------------------------------------------------------------------
-
 
 
 class Rule:
@@ -201,25 +155,8 @@ class Rule:
 
         return rule
 
-    def pretty_print(self, level=0):
-        tabs = (level) * '    '
-        ret = ""
-        ret += tabs + self.lname
-        for option in self.options:
-            ret += option.pretty_print(level=level + 1)
-        return ret
-
-    def graphviz_id(self):
-        return "Rule {lname}".format(lname=self.lname)
-
-    def to_graphviz(self, graph):
-        for opt in self.options:
-            graph.edge(self.graphviz_id(), opt.graphviz_id())
-            opt.to_graphviz(graph)
-
 
 # ----------------------------------------------------------------------------------------------------
-
 
 
 class Tree:
@@ -231,12 +168,12 @@ class Tree:
 
     def next(self, idx):
         if idx + 1 < len(self.option.conjuncts):
-            return (self, idx + 1)
+            return self, idx + 1
         else:
             if self.parent:
                 return self.parent.next(self.parent_idx)
             else:
-                return (None, 0)
+                return None, 0
 
     def add_subtree(self, idx, tree):
         tree.parent = self
@@ -248,23 +185,8 @@ class Tree:
         # TODO: Make this print like a tree
         return str(zip(self.option.conjuncts, self.subtrees))
 
-    def pretty_print(self, level=0):
-        # print self, level
-        # tabs = (level-1)*'    ' + "│   ├───"
-        tabs = (level) * '    ' + "└───"
-        # tabs = "\t" * level #
-        ret = ""  # "#tabs + self.option.pretty_print(level=level)
-        for conjunct, subtree in zip(self.option.conjuncts, self.subtrees):
-            ret += tabs + conjunct.pretty_print() + "\n"
-            if hasattr(subtree, "pretty_print"):
-                ret += subtree.pretty_print(level=level + 1)
-                # ret += "\n"
-
-        return ret
-
 
 # ----------------------------------------------------------------------------------------------------
-
 
 
 def parse_next_atom(s):
@@ -280,14 +202,14 @@ def parse_next_atom(s):
     for i in range(0, len(s)):
         c = s[i]
         if c == ' ':
-            return (s[:i], "", s[i:].strip())
+            return s[:i], "", s[i:].strip()
         elif c == '[':
             j = s.find("]", i)
             if j < 0:
                 raise Exception
-            return (s[:i], s[i + 1:j], s[j + 1:].strip())
+            return s[:i], s[i + 1:j], s[j + 1:].strip()
 
-    return (s, "", "")
+    return s, "", ""
 
 
 # ----------------------------------------------------------------------------------------------------
@@ -365,8 +287,6 @@ class GrammarParser:
         for opt in rule.options:
             T = Tree(opt)
             if self._parse((T, 0), words):
-                if debug:
-                    print T.pretty_print()
                 # Simply take the first tree that successfully parses
                 semantics_str = self.get_semantics(T).replace("<", "[").replace(">", "]")
                 try:
@@ -377,8 +297,8 @@ class GrammarParser:
 
         return False
 
-    def _parse(self, TIdx, words):
-        (T, idx) = TIdx
+    def _parse(self, T_idx, words):
+        (T, idx) = T_idx
 
         if not T:
             return words == []
@@ -389,7 +309,7 @@ class GrammarParser:
         conj = T.option.conjuncts[idx]
 
         if conj.is_variable:
-            if not conj.name in self.rules:
+            if conj.name not in self.rules:
                 return False
             options = self.rules[conj.name].options
 
@@ -416,7 +336,7 @@ class GrammarParser:
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     def next_word(self, target, words):
-        if not target in self.rules:
+        if target not in self.rules:
             return False
 
         rule = self.rules[target]
@@ -447,7 +367,7 @@ class GrammarParser:
             options = self.get_completion_function(func_name)(words)
 
         else:
-            if words == []:
+            if words:
                 return [conj.name]
             elif conj.name == words[0]:
                 return self._next_word(T.next(idx), words[1:])
@@ -466,35 +386,6 @@ class GrammarParser:
 
     def get_completion_function(self, func_name):
         return self.functions[func_name]
-
-    def graphviz_id(self):
-        return "GrammarParser"
-
-    def to_graphviz(self, graph):
-        for name, rule in self.rules.iteritems():
-            graph.edge(self.graphviz_id(), rule.graphviz_id())
-            rule.to_graphviz(graph)
-
-    def visualize_options(self, graph, target_rule, previous_words=None, depth=2):
-        previous_words = [] if not previous_words else previous_words
-
-        import itertools
-        colors = itertools.cycle(["blue", "green", "red", "cyan", "magenta", "black", "purple", "orange"])
-
-        if previous_words:
-            previous_word = previous_words[-1]
-        else:
-            previous_word = target_rule
-
-        graph.node(previous_word)
-        next_words = set(self.next_word(target_rule, previous_words))
-
-        if next_words and depth:
-            # print next_words
-
-            for next_word in next_words:
-                graph.edge(previous_word, next_word, color=colors.next())
-                self.visualize_options(graph, target_rule, previous_words + [next_word], depth=depth - 1)
 
     def get_unwrapped(self, lname):
         if lname not in self.rules:
@@ -534,31 +425,3 @@ class GrammarParser:
                 spec = spec.replace(option, random.choice(option[1:-1].split("|")), 1)
 
         return spec
-
-
-class Visualizer(object):
-    def __init__(self, grammarfile):
-        self.parser = GrammarParser.fromfile(grammarfile)
-
-    def get_completion_function(self, name):
-        return lambda x: [Option(name, [Conjunct(name.upper())])]
-
-    def test(self, rule, depth):
-        import graphviz
-        self.parser.has_completion_function = lambda func_name: True
-        self.parser.get_completion_function = self.get_completion_function
-
-        g = graphviz.Digraph(strict=True)
-        self.parser.visualize_options(g, rule, depth=int(depth))
-        g.render('options', view=True)
-
-
-if __name__ == "__main__":
-    import sys
-
-    grammar_file = sys.argv[1]
-    rule = sys.argv[2]
-    depth = int(sys.argv[3])
-
-    tester = Visualizer(grammar_file)
-    tester.test(rule, depth=depth)
